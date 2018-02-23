1300c49c8fe960c129d2e04cb3091106eacca779
Mark Biek <support@janustech.net>
2018-02-23 15:24:43 -0500

    Updated build and folder structure
    Use webpack for faster builds and organize the react stuff into a more
    sane format

--- assets/js/components/Test/index.js
+++ assets/js/components/Test/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { connect } from 'react-redux';
 import Immutable from 'immutable';
-import store from '../store.js';
+import store from '../../store';
 
 class TestComponent extends React.Component {
     componentWillMount () {
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,47 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'build/js');
-var APP_DIR = path.resolve(__dirname, 'assets/js');
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname, 'assets');
 
-var config = {
-    entry: APP_DIR + '/index.jsx',
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].css"
+});
+
+const config = {
+    entry: [APP_DIR + '/js/app.js', APP_DIR + '/scss/app.scss'],
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
     devtool: 'source-map',
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?/,
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'sass-loader'
+                    }]
+                })
+            },
+            {
+                test: /\.js?/,
                 include: APP_DIR,
-                loader: 'babel'
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'es2015', 'es2016', 'react'],
+                    plugins: ['transform-regenerator']
+                }
             }
         ]
-    }
+    },
+    plugins: [
+        extractSass
+    ]
 };
 
 module.exports = config;
